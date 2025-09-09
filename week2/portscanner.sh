48c3a09ac2669c97f4e77021bb384c91002b6602
ryan <ryan.starr@mymail.champlain.edu>
2025-09-09 13:23:48 -0400

    week2 script

---
+++ week2/portscanner.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+hostfile=$1
+portfile=$2
+for host in $(cat $hostfile); do
+	for port in $(cat $portfile); do
+		timeout .1 bash -c "echo >/dev/tcp/$host/$port" 2>/dev/null &&
+			echo "$host:$port"
+	done
+done
