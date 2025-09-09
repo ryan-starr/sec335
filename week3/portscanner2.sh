a07485ea8cdb05df8252ba09e0ff5e199c96d725
ryan <ryan.starr@mymail.champlain.edu>
2025-09-09 14:08:02 -0400

    week3 scripts

--- week3/portscanner2.sh
+++ week3/portscanner2.sh
@@ -6,9 +6,6 @@ port=$2
 echo "ip,port"
 
 for i in $(seq 1 254); do
-	ip="$prefix.$ip"
-	timeout 1 bash -c "echo >/dev/tcp/$ip/$port" 2>/dev/null && echo "$ip,$port"
+	ip="$prefix.$i"
+	timeout .1 bash -c "echo >/dev/tcp/$ip/$port" 2>/dev/null && echo "$ip,$port"
 done
-
-# Force the deliverable output line (only once)
-# echo "10.0.5.22,53"
